c2e50c307910e0ec1496168fb902f145761e2fe9
cvertiz <cvertiz@growbiz.la>
2025-03-03 17:25:19 -0500

    onSearch

--- app/iteractions-items/webapp/controller/Main.controller.js
+++ app/iteractions-items/webapp/controller/Main.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+
+], (Controller, Filter, FilterOperator, Sorter, MessageToast, MessageBox) => {
     "use strict";
 
     return Controller.extend("myhanapp.iteractionsitems.controller.Main", {
-        onInit: function () {
+
+        _loadViewModel: function () {
             var oModel = this.getOwnerComponent().getModel();
             console.log(oModel);
             this.getView().setModel(oModel);
+        },
+        onInit: function () {
+            this._loadViewModel();
+        },
+        onSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("newValue"); // Obtiene el valor ingresado
+            var oTable = this.getView().byId("itemsTable"); // Referencia a la tabla
+            var oBinding = oTable.getBinding("items"); // Obtiene el binding de la tabla
+
+            if (oBinding) {
+                var aFilters = [];
+                if (sQuery) {
+                    aFilters.push(new Filter("text", FilterOperator.Contains, sQuery));
+                }
+                oBinding.filter(aFilters);
+            }
         }
 
     });
 });
-
